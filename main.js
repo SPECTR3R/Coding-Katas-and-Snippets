b26868a9a60f2d0de636264afb59478135220ddd
Armando del Rio <d-rio@outlook.com>
2020-04-24 18:38:12 -0500

    added some comments

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-/*jshint esversion: 6 */
+// Half diamond star pattern
 
-let printStars = howMany => console.log(" *".repeat(howMany));
+let printStars = howMany => console.log(' *'.repeat(howMany));
 
-[1, 2, 3, 4, 5, 6, 7].forEach(
-    function (num, it, arr) {
-        let p = Math.round(arr.length / 2) - 1;
-        if (it < p) {
-            printStars(arr[it] + num-1);
-            printStars(arr[it] + num);
-        } else if (it === p && (arr.length - 1) % 2 !== 0) {
-            printStars(arr[arr.length - 1] - 1);
-        } else if (it === p && (arr.length - 1) % 2 == 0) {
-            printStars(arr[arr.length - 1]);
-        } else {
-            printStars(2 * (arr.length - it));
-            printStars(2 * (arr.length - it) - 1);
-        }
-    }
-);
+
+[1, 2, 3, 4, 5, 6, 7].forEach(function (num, it, arr) {
+  let p = Math.round(arr.length / 2) - 1;
+  if (it < p) {
+    printStars(arr[it] + num - 1);
+    printStars(arr[it] + num);
+  } else if (it === p && (arr.length - 1) % 2 !== 0) {
+    printStars(arr[arr.length - 1] - 1);
+  } else if (it === p && (arr.length - 1) % 2 == 0) {
+    printStars(arr[arr.length - 1]);
+  } else {
+    printStars(2 * (arr.length - it));
+    printStars(2 * (arr.length - it) - 1);
+  }
+});
